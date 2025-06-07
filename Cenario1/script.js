086a7ea20b4faaae83158b19b31724c3e393c244
unknown <luiz.r@ufms.br>
2025-06-07 14:52:52 -0400

    Primeira atualização da pagina inicial

--- Cenario1/script.js
+++ Cenario1/script.js
@@ -1,9 +1,1 @@
-let contador = 0;
 
-const botao = document.getElementById('botao');
-const display = document.getElementById('contador');
-
-botao.addEventListener('click', () => {
-  contador++;
-  display.textContent = contador;
-});
