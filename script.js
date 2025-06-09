409be15de7572a567d464bc4c5c59b55ca3f8b16
unknown <luiz.r@ufms.br>
2025-06-09 09:18:17 -0400

    Removendo template de contador presente na main pois não é mais relevante para o decorrer da prova

--- script.js
+++
@@ -1,9 +0,0 @@
-let contador = 0;
-
-const botao = document.getElementById('botao');
-const display = document.getElementById('contador');
-
-botao.addEventListener('click', () => {
-  contador++;
-  display.textContent = contador;
-});
